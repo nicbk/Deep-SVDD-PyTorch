4788f7452a22818899885a1413c96b4a5e585375
Nicolás Kennedy <nicolas@nicbk.com>
2023-09-25 12:20:54 -0700

    Fix: Change CNN kernel size

--- src/networks/cifar10_LeNet.py
+++ src/networks/cifar10_LeNet.py
@@ -13,11 +13,11 @@ class CIFAR10_LeNet(BaseNet):
         self.rep_dim = 128
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.conv1 = nn.Conv2d(3, 32, 5, bias=False, padding=2)
+        self.conv1 = nn.Conv2d(3, 32, 28, bias=False, padding=2)
         self.bn2d1 = nn.BatchNorm2d(32, eps=1e-04, affine=False)
-        self.conv2 = nn.Conv2d(32, 64, 5, bias=False, padding=2)
+        self.conv2 = nn.Conv2d(32, 64, 28, bias=False, padding=2)
         self.bn2d2 = nn.BatchNorm2d(64, eps=1e-04, affine=False)
-        self.conv3 = nn.Conv2d(64, 128, 5, bias=False, padding=2)
+        self.conv3 = nn.Conv2d(64, 128, 28, bias=False, padding=2)
         self.bn2d3 = nn.BatchNorm2d(128, eps=1e-04, affine=False)
         self.fc1 = nn.Linear(128 * 4 * 4, self.rep_dim, bias=False)
 
@@ -42,29 +42,29 @@ class CIFAR10_LeNet_Autoencoder(BaseNet):
         self.pool = nn.MaxPool2d(2, 2)
 
         # Encoder (must match the Deep SVDD network above)
-        self.conv1 = nn.Conv2d(3, 32, 5, bias=False, padding=2)
+        self.conv1 = nn.Conv2d(3, 32, 28, bias=False, padding=2)
         nn.init.xavier_uniform_(self.conv1.weight, gain=nn.init.calculate_gain('leaky_relu'))
         self.bn2d1 = nn.BatchNorm2d(32, eps=1e-04, affine=False)
-        self.conv2 = nn.Conv2d(32, 64, 5, bias=False, padding=2)
+        self.conv2 = nn.Conv2d(32, 64, 28, bias=False, padding=2)
         nn.init.xavier_uniform_(self.conv2.weight, gain=nn.init.calculate_gain('leaky_relu'))
         self.bn2d2 = nn.BatchNorm2d(64, eps=1e-04, affine=False)
-        self.conv3 = nn.Conv2d(64, 128, 5, bias=False, padding=2)
+        self.conv3 = nn.Conv2d(64, 128, 28, bias=False, padding=2)
         nn.init.xavier_uniform_(self.conv3.weight, gain=nn.init.calculate_gain('leaky_relu'))
         self.bn2d3 = nn.BatchNorm2d(128, eps=1e-04, affine=False)
         self.fc1 = nn.Linear(128 * 4 * 4, self.rep_dim, bias=False)
         self.bn1d = nn.BatchNorm1d(self.rep_dim, eps=1e-04, affine=False)
 
         # Decoder
-        self.deconv1 = nn.ConvTranspose2d(int(self.rep_dim / (4 * 4)), 128, 5, bias=False, padding=2)
+        self.deconv1 = nn.ConvTranspose2d(int(self.rep_dim / (4 * 4)), 128, 28, bias=False, padding=2)
         nn.init.xavier_uniform_(self.deconv1.weight, gain=nn.init.calculate_gain('leaky_relu'))
         self.bn2d4 = nn.BatchNorm2d(128, eps=1e-04, affine=False)
-        self.deconv2 = nn.ConvTranspose2d(128, 64, 5, bias=False, padding=2)
+        self.deconv2 = nn.ConvTranspose2d(128, 64, 28, bias=False, padding=2)
         nn.init.xavier_uniform_(self.deconv2.weight, gain=nn.init.calculate_gain('leaky_relu'))
         self.bn2d5 = nn.BatchNorm2d(64, eps=1e-04, affine=False)
-        self.deconv3 = nn.ConvTranspose2d(64, 32, 5, bias=False, padding=2)
+        self.deconv3 = nn.ConvTranspose2d(64, 32, 28, bias=False, padding=2)
         nn.init.xavier_uniform_(self.deconv3.weight, gain=nn.init.calculate_gain('leaky_relu'))
         self.bn2d6 = nn.BatchNorm2d(32, eps=1e-04, affine=False)
-        self.deconv4 = nn.ConvTranspose2d(32, 3, 5, bias=False, padding=2)
+        self.deconv4 = nn.ConvTranspose2d(32, 3, 28, bias=False, padding=2)
         nn.init.xavier_uniform_(self.deconv4.weight, gain=nn.init.calculate_gain('leaky_relu'))
 
     def forward(self, x):
