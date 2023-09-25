b7fdf311d549a08ebe59bcaf5b9bcfcfeef61970
Nicolás Kennedy <nicolas@nicbk.com>
2023-09-25 00:31:24 -0700

    Add: CelebA Dataset

--- src/datasets/celeba.py
+++ src/datasets/celeba.py
@@ -5,13 +5,14 @@ from base.torchvision_dataset import TorchvisionDataset
 from .preprocessing import get_target_label_idx, global_contrast_normalization
 from typing import Any, Callable, Optional, Tuple, Union
 
+import os
 import warnings
 import torchvision.transforms as transforms
 
 
 class CelebA_Dataset(TorchvisionDataset):
 
-    def __init__(self, root: str, normal_class=5):
+    def __init__(self, root: str, normal_class=31):
         super().__init__(root)
 
         self.n_classes = 2  # 0: normal, 1: outlier
@@ -19,19 +20,25 @@ class CelebA_Dataset(TorchvisionDataset):
         self.outlier_classes = list(range(0, 40))
         self.outlier_classes.remove(normal_class)
 
-        # Pre-computed min and max values (after applying GCN) from train data per class
-        min_max = [(-28.94083453598571, 13.802961825439636),
-                   (-6.681770233365245, 9.158067708230273),
-                   (-34.924463588638204, 14.419298165027628),
-                   (-10.599172931391799, 11.093187820377565),
-                   (-11.945022995801637, 10.628045447867583),
-                   (-9.691969487694928, 8.948326776180823),
-                   (-9.174940012342555, 13.847014686472365),
-                   (-6.876682005899029, 12.282371383343161),
-                   (-15.603507135507172, 15.2464923804279),
-                   (-6.132882973622672, 8.046098172351265)]
+        train_set_full = MyCelebA(root=root, split='train', target_type='attr', download=True, transform=None, target_transform=None)
 
-        # CIFAR-10 preprocessing: GCN (with L1 norm) and min-max feature scaling to [0,1]
+        MIN = []
+        MAX = []
+        for normal_classes in range(40):
+            train_idx_normal = get_target_label_idx(train_set_full.train_labels.clone().data.cpu().numpy(), normal_classes)
+            train_set = Subset(train_set_full, train_idx_normal)
+
+            _min_ = []
+            _max_ = []
+            for idx in train_set.indices:
+                gcm = global_contrast_normalization(train_set.dataset.data[idx].float(), 'l1')
+                _min_.append(gcm.min())
+                _max_.append(gcm.max())
+            MIN.append(np.min(_min_))
+            MAX.append(np.max(_max_))
+        min_max = list(zip(MIN, MAX)))
+
+        # CelebA preprocessing: GCN (with L1 norm) and min-max feature scaling to [0,1]
         transform = transforms.Compose([transforms.ToTensor(),
                                         transforms.Lambda(lambda x: global_contrast_normalization(x, scale='l1')),
                                         transforms.Normalize([min_max[normal_class][0]] * 3,
@@ -39,14 +46,12 @@ class CelebA_Dataset(TorchvisionDataset):
 
         target_transform = transforms.Lambda(lambda x: int(x in self.outlier_classes))
 
-        train_set = MyCIFAR10(root=self.root, train=True, download=True,
-                              transform=transform, target_transform=target_transform)
+        train_set = MyCelebA(root=self.root, split='train', target_type='attr', download=True, transform=transform, target_transform=target_transform)
         # Subset train set to normal class
         train_idx_normal = get_target_label_idx(train_set.targets, self.normal_classes)
         self.train_set = Subset(train_set, train_idx_normal)
 
-        self.test_set = MyCIFAR10(root=self.root, train=False, download=True,
-                                  transform=transform, target_transform=target_transform)
+        self.test_set = MyCelebA(root=self.root, split='test', target_type='attr', download=True, transform=transform, target_transform=target_transform)
 
 
 class MyCelebA(CelebA):
@@ -74,25 +79,40 @@ class MyCelebA(CelebA):
         return self.data
 
     def __init__(self, root: str, split: str = 'train', target_type: Union[List[str], str] = 'attr', transform: Optional[Callable] = None, target_transform: Optional[Callable] = None, download: bool = False):
-        super().__init__(root, train, transform, target_transform, download)
+        super().__init__(root, split, target_type, transform, target_transform, download)
 
     def __getitem__(self, index):
-        """Override the original method of the CIFAR10 class.
+        """Override the original method of the CelebA class.
         Args:
             index (int): Index
         Returns:
             triple: (image, target, index) where target is index of the target class.
         """
-        img, target = self.data[index], self.targets[index]
+        X = Image.open(os.path.join(self.root, self.base_folder, "img_align_celeba", self.filename[index]))
 
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
-        img = Image.fromarray(img)
+        target: Any = []
+        for t in self.target_type:
+            if t == "attr":
+                target.append(self.attr[index, :])
+            elif t == "identity":
+                target.append(self.identity[index, 0])
+            elif t == "bbox":
+                target.append(self.bbox[index, :])
+            elif t == "landmarks":
+                target.append(self.landmarks_align[index, :])
+            else:
+                # TODO: refactor with utils.verify_str_arg
+                raise ValueError(f'Target type "{t}" is not recognized.')
 
         if self.transform is not None:
-            img = self.transform(img)
+            X = self.transform(X)
 
-        if self.target_transform is not None:
-            target = self.target_transform(target)
+        if target:
+            target = tuple(target) if len(target) > 1 else target[0]
 
-        return img, target, index  # only line changed
+            if self.target_transform is not None:
+                target = self.target_transform(target)
+        else:
+            target = None
+
+        return X, target, index  # only line changed
--- src/networks/main.py
+++ src/networks/main.py
@@ -1,12 +1,13 @@
 from .mnist_LeNet import MNIST_LeNet, MNIST_LeNet_Autoencoder
 from .cifar10_LeNet import CIFAR10_LeNet, CIFAR10_LeNet_Autoencoder
 from .cifar10_LeNet_elu import CIFAR10_LeNet_ELU, CIFAR10_LeNet_ELU_Autoencoder
+from .celeba import CelebA_Net, CelebA_Net_Autoencoder
 
 
 def build_network(net_name):
     """Builds the neural network."""
 
-    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet', 'cifar10_LeNet_ELU')
+    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet', 'cifar10_LeNet_ELU', 'celeba')
     assert net_name in implemented_networks
 
     net = None
@@ -20,13 +21,16 @@ def build_network(net_name):
     if net_name == 'cifar10_LeNet_ELU':
         net = CIFAR10_LeNet_ELU()
 
+    if net_name == 'celeba':
+        net = CelebA_Net()
+
     return net
 
 
 def build_autoencoder(net_name):
     """Builds the corresponding autoencoder network."""
 
-    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet', 'cifar10_LeNet_ELU')
+    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet', 'cifar10_LeNet_ELU', 'celeba')
     assert net_name in implemented_networks
 
     ae_net = None
@@ -39,5 +43,8 @@ def build_autoencoder(net_name):
 
     if net_name == 'cifar10_LeNet_ELU':
         ae_net = CIFAR10_LeNet_ELU_Autoencoder()
+
+    if net_name == 'celeba':
+        ae_net = CelebA_Net_Autoencoder()
 
     return ae_net
