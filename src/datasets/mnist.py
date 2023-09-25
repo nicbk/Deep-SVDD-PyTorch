730308be186e528c189135fa34a2b5b7fa972eb2
Nicolás Kennedy <nicolas@nicbk.com>
2023-09-24 22:50:52 -0700

    Fix: "train-labels" changed to "targets" for CIFAR10 class

--- src/datasets/mnist.py
+++ src/datasets/mnist.py
@@ -60,10 +60,7 @@ class MyMNIST(MNIST):
         Returns:
             triple: (image, target, index) where target is index of the target class.
         """
-        if self.train:
-            img, target = self.train_data[index], self.train_labels[index]
-        else:
-            img, target = self.test_data[index], self.test_labels[index]
+        img, target = self.data[index], self.targets[index]
 
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
