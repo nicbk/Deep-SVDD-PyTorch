1064716e2987d28354774ce8ccc5152292a0018a
Nicolás Kennedy <nicolas@nicbk.com>
2023-09-25 00:53:24 -0700

    Fix

--- src/datasets/main.py
+++ src/datasets/main.py
@@ -7,7 +7,7 @@ def load_dataset(dataset_name, data_path, normal_class):
     """Loads the dataset."""
 
     implemented_datasets = ('mnist', 'cifar10', 'celeba')
-    assert dataset_name == 'celeba'
+    assert dataset_name in implemented_datasets
 
     dataset = None
 
