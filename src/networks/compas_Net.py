955a7aff8d6f3c7e00450b7f357224abfef66dc9
Nicolás Kennedy <nicolas@nicbk.com>
2024-01-16 20:53:49 -0800

    Fix

--- src/networks/compas_Net.py
+++ src/networks/compas_Net.py
@@ -7,9 +7,10 @@ from base.base_net import BaseNet
 class COMPAS_Net(BaseNet):
     def __init__(self, num_tags):
         super().__init__()
+        self.rep_dim = 16
 
         self.e_hidden1 = nn.Linear(num_tags, 32)
-        self.e_hidden2 = nn.Linear(32, 16)
+        self.e_hidden2 = nn.Linear(32, self.rep_dim)
 
     def forward(self, x):
         x = F.leaky_relu(self.e_hidden1(x))
@@ -21,11 +22,12 @@ class COMPAS_Net_Autoencoder(BaseNet):
 
     def __init__(self, num_tags):
         super().__init__()
+        self.rep_dim = 16
 
         self.e_hidden1 = nn.Linear(num_tags, 32)
-        self.e_hidden2 = nn.Linear(32, 16)
+        self.e_hidden2 = nn.Linear(32, self.rep_dim)
 
-        self.d_hidden1 = nn.Linear(16, 32)
+        self.d_hidden1 = nn.Linear(self.rep_dim, 32)
         self.d_hidden2 = nn.Linear(32, num_tags)
 
     def forward(self, x):
