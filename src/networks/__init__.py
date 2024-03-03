c635436c41a1811b3e0958c2fbc592933394cc3a
Nicolás Kennedy <nicolas@nicbk.com>
2024-03-02 21:38:29 -0800

    Fix

--- src/networks/__init__.py
+++ src/networks/__init__.py
@@ -3,4 +3,3 @@ from .mnist_LeNet import MNIST_LeNet, MNIST_LeNet_Autoencoder
 from .cifar10_LeNet import CIFAR10_LeNet, CIFAR10_LeNet_Autoencoder
 from .cifar10_LeNet_elu import CIFAR10_LeNet_ELU, CIFAR10_LeNet_ELU_Autoencoder
 from .celeba_Net import CelebA_Net, CelebA_Net_Autoencoder
-from .celeba_vae_Net import CelebA_VAE_Net, CelebA_VAE_Net_Autoencoder
